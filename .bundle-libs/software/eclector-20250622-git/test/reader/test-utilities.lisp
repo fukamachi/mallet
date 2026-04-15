23b250356efd4a735c038034617671979c4969c9
Eitaro Fukamachi <e.arrows@gmail.com>
2026-04-15 11:46:09 +0900

    Regenerate bundled libraries to include cl-interpol
    The standalone executable loads .bundle-libs/setup.lisp at runtime when
    .qlot/ isn't present, so adding cl-interpol as an ASDF dependency means
    the bundle has to be refreshed too. Includes its transitive deps
    (cl-unicode, named-readtables) and the dist-bumped versions of the
    existing libraries.

--- .bundle-libs/software/eclector-20250622-git/test/reader/test-utilities.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:eclector.reader.test)
-
-;;; Client for testing SHARPSIGN-S
-
-(defclass sharpsign-s-client () ())
-
-(defmethod eclector.reader:make-structure-instance
-    ((client sharpsign-s-client) (name t) (initargs t))
-  (list* name initargs))
